58c4d8210a0630794e7c0b6e25093335a3bb45b6
Muhammed Aflah TK <aflutech2@gmail.com>
2026-01-09 16:18:29 +0530

    completed for now, made code cleaner, finished navbar,card, and the searching feature

--- tsLearning/src/App.tsx
+++ tsLearning/src/App.tsx
@@ -1,56 +1,39 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Person from './Person';
-import { usersArr } from './utils/constants';
-import Navbar from './Navbar';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Person from "./Person";
+import { usersArr } from "./utils/constants";
+import Navbar from "./Navbar";
 
 function App() {
-
   let [isUser, setIsUser] = useState<object[]>([]);
-  let [searchText,setSearch] = useState<string>('');
+  let [searchText, setSearch] = useState<string>("");
 
-  const fetchUser =  () : void => {
+  const fetchUser = (): void => {
     let res: object[] = usersArr;
-    if(searchText !== '') {
-
-      const filteredPersons:object[] = res.filter((person) => {
-        if(person?.name.toLowerCase().includes(searchText.toLocaleLowerCase())) return person;
-      })
-      console.log()
-      setIsUser(filteredPersons)
+    if (searchText !== "") {
+      const filteredPersons: object[] = res.filter((person) => {
+        if (person?.name.toLowerCase().includes(searchText.toLocaleLowerCase())) return person;
+      });
+      setIsUser(filteredPersons);
     } else {
-
       setIsUser(res);
     }
-  }
+  };
 
-  useEffect( ()  => {
-    fetchUser()
-    console.log('h')
-  },[searchText])
+  useEffect(() => {
+    fetchUser();
+  }, [searchText]);
 
   return (
-    <div id='theRoot'>
-      <section id='navId'>
-      {
-        isUser &&
-        <Navbar setSearch={setSearch} searchText={searchText} />
-      }
-      </section>
-    <main id='main'>
-    { isUser &&
-      isUser.map((ele : any) => (
+    <div id="theRoot">
+      <section id="navId">{isUser && <Navbar setSearch={setSearch} searchText={searchText} />}</section>
+      <main id="main">
+        {isUser && isUser.map((ele: any) => <Person key={ele.name} name={ele.name} about={ele.about} place={ele.place} img={ele.photoUrl} />)}
 
-        <Person key={ele.name} name={ele.name} about={ele.about} place={ele.place} img={ele.photoUrl} />
-      ))
-    }
-
-    {isUser.length == 0 && 
-    <h3>No Charecter Found!!</h3>
-    }
-    </main>
+        {isUser.length == 0 && <h3>No Charecter Found!!</h3>}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
